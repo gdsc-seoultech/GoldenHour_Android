64b9ff50c03f26ef46658a94de72f2f19975e8d5
leeeha <jxlhe46@seoultech.ac.kr>
2023-01-30 17:36:57 +0900

    feat: 스플래시 화면 구현 (로티 애니메이션)

---
+++ app/src/main/java/com/gdsc/goldenhour/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.gdsc.goldenhour
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.gdsc.goldenhour.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashIcon.playAnimation()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2300)
+    }
+}
