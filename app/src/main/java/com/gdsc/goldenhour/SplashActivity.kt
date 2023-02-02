0d1f831971330bfb1c9b2ccb3c47b6be3c9a6e8b
leeeha <jxlhe46@seoultech.ac.kr>
2023-02-02 13:46:02 +0900

    design: 스플래쉬, 상태바, 앱바, 네비게이션바 색상 변경

--- app/src/main/java/com/gdsc/goldenhour/SplashActivity.kt
+++ app/src/main/java/com/gdsc/goldenhour/SplashActivity.kt
@@ -19,6 +19,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        },2300)
+        },2500)
     }
 }
