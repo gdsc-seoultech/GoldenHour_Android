e614092a6c2d68cc14956dd5fdc75ac72a6bc408
leeeha <jxlhe46@seoultech.ac.kr>
2023-03-16 21:57:54 +0900

    feat(체크리스트 일반모드): 구호물자 리스트뷰, FAB

--- app/src/main/java/com/gdsc/goldenhour/view/checklist/normal/NormalTwoFragment.kt
+++ app/src/main/java/com/gdsc/goldenhour/view/checklist/normal/NormalTwoFragment.kt
@@ -2,12 +2,18 @@ package com.gdsc.goldenhour.view.checklist.normal
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import com.gdsc.goldenhour.binding.BindingFragment
-import com.gdsc.goldenhour.databinding.FragmentNormalThreeBinding
+import com.gdsc.goldenhour.databinding.FragmentNormalTwoBinding
 
+// ArrayAdapter
 class NormalTwoFragment :
-    BindingFragment<FragmentNormalThreeBinding>(FragmentNormalThreeBinding::inflate) {
+    BindingFragment<FragmentNormalTwoBinding>(FragmentNormalTwoBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val data = listOf("물", "방석")
+        binding.lvEmergencyGoods.adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, data)
     }
 }
