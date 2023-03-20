5886957cd39db1065d677af3d7fe029028be2413
leeeha <jxlhe46@seoultech.ac.kr>
2023-03-21 00:00:24 +0900

    remove(체크리스트 재난모드): 사용하지 않는 import문 삭제

--- app/src/main/java/com/gdsc/goldenhour/view/checklist/disaster/DisasterThreeFragment.kt
+++ app/src/main/java/com/gdsc/goldenhour/view/checklist/disaster/DisasterThreeFragment.kt
@@ -1,16 +1,10 @@
 package com.gdsc.goldenhour.view.checklist.disaster
 
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.gdsc.goldenhour.binding.BindingFragment
 import com.gdsc.goldenhour.databinding.FragmentDisasterThreeBinding
