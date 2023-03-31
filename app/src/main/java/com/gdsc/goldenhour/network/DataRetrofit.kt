f2e4ee60734a30962bc18402fe447b12a31fbcb7
leeeha <jxlhe46@seoultech.ac.kr>
2023-03-31 21:09:56 +0900

    remove: 사용하지 않는 okHttpClient 삭제

--- app/src/main/java/com/gdsc/goldenhour/network/DataRetrofit.kt
+++ app/src/main/java/com/gdsc/goldenhour/network/DataRetrofit.kt
@@ -2,21 +2,9 @@ package com.gdsc.goldenhour.network
 
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object DataRetrofit {
-
-    private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        setLevel(HttpLoggingInterceptor.Level.BODY)
-    }
-
-    private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
-
-    // 맵
-
     private const val DATA_API_URL = "http://apis.data.go.kr"
 
     val parser = TikXml.Builder()
@@ -26,9 +14,7 @@ object DataRetrofit {
     val retrofitAPI = Retrofit.Builder()
         .baseUrl(DATA_API_URL)
         .addConverterFactory(TikXmlConverterFactory.create(parser))
-        .client(RetrofitObject.okHttpClient.build())
         .build()
 
     val dataService: DataApi = retrofitAPI.create(DataApi::class.java)
-
 }
